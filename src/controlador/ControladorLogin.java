f4e4c8eb775872ffd45a586f01fe4a38c36d001c
AngelGio <85459682+AngelGio@users.noreply.github.com>
2023-05-31 11:30:24 -0500

    Refactorización - Login

--- src/controlador/ControladorLogin.java
+++ src/controlador/ControladorLogin.java
@@ -6,27 +6,18 @@
 package controlador;
 
 import javax.swing.JOptionPane;
+
 import modelo.dao.UsuarioDAO;
 import modelo.vo.Usuario;
 import vista.VentanaLogin;
 import vista.VentanaPrincipal;
 
-/**
- *
- * @author krypt97
- */
 public class ControladorLogin {
-
-    // ATRIBUTOS DE CLASE
     private VentanaLogin miVentanaLogin;
     private VentanaPrincipal miVentanaPrincipal;
     private UsuarioDAO miUsuarioDAO;
-    private Usuario miUsuario; // usuario principal del main
-    private Usuario miUsuarioBuscado; // Variable necesaria para setear el usuario principal del main
-    private String username;
-    private String password;
+    private Usuario miUsuario;
 
-    // ENLACE VISTA
     public void setVentanaLogin(VentanaLogin miVentanaLogin) {
         this.miVentanaLogin = miVentanaLogin;
     }
@@ -35,43 +26,47 @@ public class ControladorLogin {
         this.miVentanaPrincipal = miVentanaPrincipal;
     }
 
-    // ENLACE MODELO
-    // Vo
-    public void setUsuario(Usuario miUsuario) { // Usuario (vendedor) unico por cada login
+    public void setUsuario(Usuario miUsuario) {
         this.miUsuario = miUsuario;
     }
 
-    // Dao
     public void setUsuarioDAO(UsuarioDAO miUsuarioDAO) {
         this.miUsuarioDAO = miUsuarioDAO;
     }
 
-    // MÉTODOS DE CLASE
     public void logearUsuario() {
-        username = miVentanaLogin.getUsername();
-        password = miVentanaLogin.getPassword();
-        miUsuarioBuscado = miUsuarioDAO.buscarUsuarioUsername(username);
-        if (miUsuarioBuscado != null) {
-            if (username.equals(miUsuarioBuscado.getUsername())) {
-                if (password.equals(miUsuarioBuscado.getPassword())) {
-                    // Setea usuario principal del main
-                    miUsuario.setDniUsuario(miUsuarioBuscado.getDniUsuario());
-                    miUsuario.setUsername(username);
-                    miUsuario.setPassword(password);
-                    miUsuario.setRol(miUsuarioBuscado.getRol());
-                    // Intercambia ventanas
-                    miVentanaLogin.setVisible(false);
-                    miVentanaPrincipal.setVisible(true);
-                    miVentanaPrincipal.panelHome.setVisible(true);
-                    validarPermisoUsuario();
-                } else {
-                    JOptionPane.showMessageDialog(null, "La contraseña proporcionada es incorrecta");
-                }
+        String username = miVentanaLogin.getUsername();
+        String password = miVentanaLogin.getPassword();
+        miUsuario = buscarUsuario(username);
+        if (miUsuario != null) {
+            if (validarCredenciales(password)) {
+                iniciarSesion();
+            } else {
+                mostrarMensaje("La contraseña proporcionada es incorrecta");
             }
         } else {
-            JOptionPane.showMessageDialog(null, "Usuario no registrado");
+            mostrarMensaje("Usuario no registrado");
         }
-        miVentanaLogin.limpiarCampos();        
+        limpiarCampos();
+    }
+
+    private Usuario buscarUsuario(String username) {
+        return miUsuarioDAO.buscarUsuarioUsername(username);
+    }
+
+    private boolean validarCredenciales(String password) {
+        return password.equals(miUsuario.getPassword());
+    }
+
+    private void iniciarSesion() {
+        miVentanaLogin.setVisible(false);
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.panelHome.setVisible(true);
+        validarPermisoUsuario();
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje);
     }
 
     public void cerrarAplicacion() {
@@ -89,9 +84,13 @@ public class ControladorLogin {
         }
     }
 
-    public void setLayoutBotonesVentanaPrincipal(int ver,int hor) {
+    private void setLayoutBotonesVentanaPrincipal(int ver, int hor) {
         java.awt.FlowLayout flowLayout1 = new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, ver, hor);
         flowLayout1.setAlignOnBaseline(true);
         miVentanaPrincipal.panLatBtns.setLayout(flowLayout1);
+    }
+
+    private void limpiarCampos() {
+        miVentanaLogin.limpiarCampos();
     }
 }
